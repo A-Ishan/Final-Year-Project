d35f0512cf05cc9ff59a49e31053b60258c429f0
A-Ishan <131543825+A-Ishan@users.noreply.github.com>
2025-03-15 11:58:58 +0000

    Reinitiation of project

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from authentication import views
-
-urlpatterns = [  # Corrected from `urlspatterns` to `urlpatterns`
-    path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
-]
